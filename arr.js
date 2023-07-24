06e748f634fe3bc5a66a44c0dfb1b22943a10fea
muhammd-amir-2535001 <lalkhan2636277@gmail.com>
2023-07-24 13:21:27 -0700

    add

---
+++ arr.js
@@ -0,0 +1,12 @@
+let arr = [5,2,1];
+let mytarger =arr[0]+arr[1];
+if(mytarger==7){
+    console.log("mytarget"mytarger);
+}
+else{
+    console.log("your target is not")
+}
+//target is 9
+//function targer()
+
+//[5,2,1]  target = 7   0.1
